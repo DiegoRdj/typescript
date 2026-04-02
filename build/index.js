834cfd0328f4bd7e05b6c8e43ca137bb989c5b03
DiegoRdj <diegorolimdejesus@gmail.com>
2026-04-02 16:54:41 -0300

    bump

---
+++ build/index.js
@@ -0,0 +1,19 @@
+// type annotations
+/*const myString: string = "dego"
+const myNumber: number = 1
+const myBoolean: boolean = true */
+/*const myName: string = "dego"
+
+function sum(a : number, b : number):number {
+    return a+b
+}
+
+    function greet(name:String, age?: number) {
+        
+    }
+
+    console.log(myName) */
+const filter = (value) => value < 0;
+const numbers = [1, 2, 3, -1, -4, -5];
+numbers.filter(filter);
+export {};
